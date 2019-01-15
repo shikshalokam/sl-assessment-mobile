b9685342a339d0839c3c550679860d1fbd65abde
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2019-01-15 06:33:02 +0530

    Date picker min max implemnetation. Rage selected integrated

---
+++ src/components/slider/slider.ts
@@ -0,0 +1,43 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { UtilsProvider } from '../../providers/utils/utils';
+
+
+@Component({
+  selector: 'slider',
+  templateUrl: 'slider.html'
+})
+export class SliderComponent {
+
+  @Input() data: any;
+  @Input() isLast: boolean;
+  @Input() isFirst: boolean;
+  @Output() nextCallBack = new EventEmitter();
+  @Output() previousCallBack = new EventEmitter()
+  @Input() evidenceId: string;
+  @Input() hideButton: boolean;
+  @Input() schoolId: string;
+  @Input() imageLocalCopyId: string;
+  @Input() generalQuestion: boolean;
+
+
+  color: string = 'light';
+  isComplete: boolean;
+
+  constructor(private utils: UtilsProvider) {
+
+    console.log('Hello SliderComponent Component');
+
+  }
+
+  ngOnInit() {
+    this.data.startTime = this.data.startTime ? this.data.startTime : Date.now();
+    this.data.value = this.data.value !== "" ? this.data.value : this.data.validation.min;
+    this.checkForValidation();
+  }
+
+  checkForValidation(): void {
+    this.data.isCompleted = this.utils.isQuestionComplete(this.data);
+    this.data.endTime = this.data.isCompleted ? Date.now() : "";
+  }
+
+}
