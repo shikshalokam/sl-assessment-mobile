c0b62ca8eb31527baca5981ed9f69e433b149768
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2019-06-21 09:38:04 +0530

    Observation details page implementattion

--- src/pages/observations/observations.module.ts
+++ src/pages/observations/observations.module.ts
@@ -4,22 +4,21 @@ import { ObservationsPage } from './observations';
 import { TranslateModule } from '@ngx-translate/core';
 import { ComponentsModule } from '../../components/components.module';
 import { AddObservationFormPageModule } from './add-observation-form/add-observation-form.module';
-// import { MyObservationPageModule } from './my-observation/my-observation.module';
-// import { ViewObservationPageModule } from './view-observation/view-observation.module';
+import { ObservationDetailsPage } from '../observation-details/observation-details';
 
 @NgModule({
   declarations: [
     ObservationsPage,
-    
+    ObservationDetailsPage
   ],
   imports: [
     TranslateModule,
     ComponentsModule,
     AddObservationFormPageModule,
-    // MyObservationPageModule,
-    // ViewObservationPageModule,
     IonicPageModule.forChild(ObservationsPage),
   ],
-  entryComponents:[]
+  entryComponents: [
+    ObservationDetailsPage
+  ]
 })
-export class ObservationsPageModule {}
+export class ObservationsPageModule { }
