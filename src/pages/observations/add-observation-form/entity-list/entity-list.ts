f94bc192f4a98d71ac449c35bede2aa543738576
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-07-04 10:44:24 +0530

    entity search new api

--- src/pages/observations/add-observation-form/entity-list/entity-list.ts
+++ src/pages/observations/add-observation-form/entity-list/entity-list.ts
@@ -19,9 +19,14 @@ export class EntityListPage {
 entityList;
 observationId ;
 searchUrl;
+limit=10;
+page=1;
+totalCount = 0;
+searchValue = "";
   selectableList: any;
-  index: any = 100 ;
+  index: any = 50 ;
   list: any = [];
+  arr = [];
   constructor(
     public navCtrl: NavController,
      public navParams: NavParams,
@@ -52,33 +57,30 @@ searchUrl;
 
   }
   
-  search(event){
+  search(event?){
+    this.searchValue = event ? event : this.searchValue;
     this.utils.startLoader();
-    this.index = 100;
-    this.apiProviders.httpGet(this.searchUrl+this.observationId+"?search="+event , success =>{
-      let arr = [] ;
-      for( let i = 0 ; i< success.result[0].metaInformation.length ; i++){
-        if(!success.result[0].metaInformation[i].selected){
-          arr.push(success.result[0].metaInformation[i])
+    this.index = 50;
+    this.apiProviders.httpGet(this.searchUrl+this.observationId+"?search="+this.searchValue+"&page="+this.page+"&limit="+this.limit , success =>{
+       this.arr = event ? [] : this.arr ;
+      for( let i = 0 ; i< success.result[0].data.length ; i++){
+        if(!success.result[0].data[i].selected){
+          this.arr.push(success.result[0].data[i])
 
         }
       }
-      console.log(JSON.stringify(arr))
-      this.selectableList =[...arr]
-      // this.selectableList.forEach( element =>{
-      //   element.selected = false;
-      // } )
-      // console.log(JSON.stringify(success.result[0]));
-
-      // console.log(JSON.stringify(this.selectableList));
-      // console.log("searched data");
-      console.log( this.index > arr.length );
-      this.index = this.index > arr.length ?  arr.length  : this.index;
-      console.log(arr.length )
-      console.log(this.index);
-      this.list = [];
-      this.list = this.selectableList.slice(0,this.index);
-      // console.log(JSON.stringify(this.list))
+       event ?  null :this.page++ ;
+      console.log(JSON.stringify(success.result[0]))
+      this.totalCount = success.result[0].count;
+      this.selectableList =[...this.arr]
+     
+      // console.log( this.index > arr.length );
+      // this.index = this.index > arr.length ?  arr.length  : this.index;
+      // console.log(arr.length )
+      // console.log(this.index);
+      // this.list = [];
+      // this.list = this.selectableList.slice(0,this.index);
+      // // console.log(JSON.stringify(this.list))
     this.utils.stopLoader();
 
 },error =>{
