a83370f420d1139d9933dd46a7908026e85f7d3c
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-07-16 14:54:34 +0530

    audio recoding in app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,8 @@ import { IndividualListingPage } from '../individual-listing/individual-listing'
 import { ObservationsPage } from '../observations/observations';
 import { SharingFeaturesProvider } from '../../providers/sharing-features/sharing-features';
 
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 declare var cordova: any;
 
 @Component({
@@ -53,10 +55,16 @@ export class HomePage {
     },
 
   ]
-
+  recording: boolean = false;
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+  audioList: any[] = [];
   constructor(public navCtrl: NavController,
     private currentUser: CurrentUserProvider,
     private network: Network,
+    private media: Media,
+  private file: File,
     private events: Events,
     private sharingFeature : SharingFeaturesProvider,
     private platform: Platform,
@@ -78,13 +86,64 @@ export class HomePage {
     }
   }
 
+
   socialSharingInApp() {
    this.sharingFeature.sharingThroughApp();
   }
-  goToPage(index) {
-    this.events.publish('navigateTab', this.allPages[index]['name'])
-  }
 
+  // getAudioList() {
+  //   if(localStorage.getItem("audiolist")) {
+  //     this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+  //     console.log(this.audioList);
+  //   }
+  // }
+
+
+  // startRecord() {
+  //   if (this.platform.is('ios')) {
+  //     this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+  //     this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+
+  //     this.audio = this.media.create(this.filePath);
+  //   } else if (this.platform.is('android')) {
+  //     this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
+  //     this.filePath = 'file:///'+this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+  //     console.log(this.filePath)
+  //     this.audio = this.media.create(this.filePath);
+  //   }
+  //   this.audio.startRecord();
+  //   this.recording = true;
+  // }
+
+  // stopRecord() {
+  //   this.audio.stopRecord();
+  //   let data = { filename: this.fileName };
+  //   this.audioList.push(data);
+  //   localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+  //   this.recording = false;
+  //   this.getAudioList();
+  // }
+
+
+  // playAudio(file,idx) {
+  //   console.log(file)
+  //   if (this.platform.is('ios')) {
+  //     this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+  //     this.audio = this.media.create(this.filePath);
+  //   } else if (this.platform.is('android')) {
+  //     this.filePath = 'file:///'+this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+  //     console.log(this.filePath)
+  //     this.audio = this.media.create(this.filePath);
+  //     console.log("audio")
+  //   }
+  //   this.audio.play();
+  //   this.audio.setVolume(1);
+  // }
+
+  goToPage(index) {
+    this.events.publish('navigateTab',this.allPages[index]['name'])
+  }
+ 
   ionViewWillLeave() {
 
   }
