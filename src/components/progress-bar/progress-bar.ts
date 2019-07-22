cc1a7b6be7006f2f940fe7b166edb127604c2dec
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-07-22 10:39:32 +0530

    progress bar in question and ecm description in section list

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -7,7 +7,9 @@ import { Component, Input } from '@angular/core';
 export class ProgressBarComponent {
 
   @Input('progress') progress = 0;
-
+  @Input('totalQuestionCount')totalQuestionCount = 0;
+  @Input('completedQuestionCount')completedQuestionCount = 0;
+  @Input() showQuestionTracker ;
   constructor() {
     // this.progress = 10
     console.log('Hello ProgressBarComponent Component');
