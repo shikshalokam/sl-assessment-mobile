020c3c5abdc3260b085290c4a8ceb00bff4e9514
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-07-30 12:50:04 +0530

    can observe a entity multiple time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -88,6 +88,7 @@ import { HintPage } from '../pages/hint/hint';
 import { Media } from '@ionic-native/media';
 import { PreviewPage } from '../pages/preview/preview';
 import { SubmissionListPage } from '../pages/submission-list/submission-list';
+import { ObservationServiceProvider } from '../providers/observation-service/observation-service';
 
 @NgModule({
   declarations: [
@@ -216,6 +217,7 @@ import { SubmissionListPage } from '../pages/submission-list/submission-list';
     HintProvider,
 
     Media,
+    ObservationServiceProvider,
   ]
 })
 export class AppModule { }
