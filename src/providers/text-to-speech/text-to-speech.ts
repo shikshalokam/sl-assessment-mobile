6563a1b709a4ab2abf21997f26b7970acc61b8a7
siba-tunerlabs <siba.sankar@tunerlabs.com>
2019-09-06 16:14:35 +0530

    speech in question text

--- src/providers/text-to-speech/text-to-speech.ts
+++ src/providers/text-to-speech/text-to-speech.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { Events } from 'ionic-angular';
 
 /*
   Generated class for the TextToSpeechProvider provider.
@@ -11,18 +12,28 @@ import { TextToSpeech } from '@ionic-native/text-to-speech';
 @Injectable()
 export class TextToSpeechProvider {
 
-  constructor(public http: HttpClient,private tts: TextToSpeech,
-    ) {
+  constructor(public http: HttpClient, private tts: TextToSpeech,
+    private events: Events
+  ) {
     console.log('Hello TextToSpeechProvider Provider');
   }
 
-  speechFromText(options){
-    options['local']= options.local ? options.local : 'en-US';
-    options['rate']= options.rate ? options.rate : 1;
-    
-    this.tts.speak(options)
-  .then(() => console.log('Success'))
-  .catch((reason: any) => console.log(reason));
-  }
+  speechFromText(options) {
+    options['local'] = options.local ? options.local : 'en-IN';
+    options['rate'] = options.rate ? options.rate : .5;
 
+    this.tts.speak(options)
+      .then(() => {
+        console.log('Success');
+        this.events.publish('speech', 'stop');
+      }
+      )
+
+      .catch((reason: any) => console.log(reason));
+  }
+  stopSpeech() {
+    this.tts.speak("").then((value) => {
+      this.events.publish('speech', 'stop');
+    }).catch();
+  }
 }
