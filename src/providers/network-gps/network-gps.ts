c50bfef2a0e38c3cb61b876e467d041cc91870ea
kiranharidas187@gmail.com <kiran@Aboves-MacBook-Pro.local>
2019-10-23 13:47:29 +0530

    Changes

--- src/providers/network-gps/network-gps.ts
+++ src/providers/network-gps/network-gps.ts
@@ -97,10 +97,10 @@ export class NetworkGpsProvider {
                 }
                 this.geolocation.getCurrentPosition(options).then((resp) => {
                   const location = `${resp.coords.latitude},${resp.coords.longitude}`;
-                  this.utils.openToast(location)
+                  // this.utils.openToast(location)
                   resolve(location)
                 }).catch((error) => {
-                  this.utils.openToast("Something went wrnog. Please try again.")
+                  this.utils.openToast("Please enable location permission to continue.")
                   reject()
                 });
               }, error => {
