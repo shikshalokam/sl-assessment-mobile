87ee6090da3d6a4b44c0c35df8c4f04079fa001c
Kiran Haridas <kiranharidas187@gmail.com>
2020-01-13 15:18:32 +0530

    Story#462 and  Story#461 (#131)

--- src/pages/observation-listing/observation-listing.ts
+++ src/pages/observation-listing/observation-listing.ts
@@ -4,6 +4,7 @@ import { ApiProvider } from '../../providers/api/api';
 import { AppConfigs } from '../../providers/appConfig';
 import { UtilsProvider } from '../../providers/utils/utils';
 import { ObservationReportsPage } from '../observation-reports/observation-reports';
+import { ActionSheetController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -13,13 +14,14 @@ import { ObservationReportsPage } from '../observation-reports/observation-repor
 export class ObservationListingPage {
 
   entityDetails;
-  observationList;
+  solutionList;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private utils: UtilsProvider,
-    private apiProvide: ApiProvider) {
+    private apiProvide: ApiProvider,
+    public actionSheetCtrl: ActionSheetController) {
   }
 
   ionViewDidLoad() {
@@ -28,29 +30,68 @@ export class ObservationListingPage {
     this.getObservationList();
   }
 
+  presentActionSheet(solutionId) {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select report type',
+      buttons: [
+        {
+          text: 'Report with score',
+          role: 'destructive',
+          handler: () => {
+            this.goToScoringReportOfSolution(solutionId)
+          }
+        },
+        {
+          text: 'Report without score',
+          handler: () => {
+            this.goToReportsOfSolution(solutionId)
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
   getObservationList() {
     const payload = {
       entityType: this.entityDetails.entityType,
       entityId: this.entityDetails._id
     }
     this.utils.startLoader();
-    this.apiProvide.httpPost(AppConfigs.observationReports.observationList, payload, success => {
-      this.observationList = success.data;
+    this.apiProvide.httpPost(AppConfigs.observationReports.solutionList, payload, success => {
+      this.solutionList = success.data;
       this.utils.stopLoader();
     }, error => {
       this.utils.stopLoader();
     }, { baseUrl: "dhiti" })
   }
 
-  goToReportsOfObservation(observationId) {
-    // this.utils.openToast("Coming soon")
+  goToReportsOfSolution(solutionId) {
     this.navCtrl.push(ObservationReportsPage,
       {
         "entityType": this.entityDetails.entityType,
         "entityId": this.entityDetails._id,
-        "observationId": observationId,
+        "solutionId": solutionId,
         "immediateChildEntityType": this.entityDetails.immediateChildEntityType
       })
+  }
+
+  goToScoringReportOfSolution(solutionId) {
+    const payload = {
+      "entityType": this.entityDetails.entityType,
+      "entityId": this.entityDetails._id,
+      "solutionId": solutionId,
+      "immediateChildEntityType": this.entityDetails.immediateChildEntityType
+    }
+    this.navCtrl.push('ReportsWithScorePage', payload);
   }
 
 }
