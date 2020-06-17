6795c90399c9d7d011907daeb81e91ee9e976678
rejneesh1 <pillairejneesh@gmail.com>
2020-06-17 14:49:32 +0530

    STORY#590-Bottom-nav,Institution

--- src/providers/storageKeys.ts
+++ src/providers/storageKeys.ts
@@ -6,11 +6,8 @@ export const storageKeys = {
   programList: "programList", // stores all programs list with solution and entities nested inside
   institutionalList: "institutionalList", // list of intitutional assessment // old flow
   individualList: "individualList", // list of individual assessment // old flow
-<<<<<<< HEAD
   institutionFlowList: "instituionFlowList ", //new institution flow data
   submissionIdArray: "submissionIdArray", //to store all downloaded submissionIds
-=======
->>>>>>> 052020-Sprint-May
   createdObservationList: "createdObservationList", // list of observational assessment // old flow
   observationSubmissionIdArr: "observationSubmissionIdArr",
 };
