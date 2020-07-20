8d2c4b7e8958fdb9a66dead8ec0cc3f6f586c26c
rejneesh1 <pillairejneesh@gmail.com>
2020-07-20 21:49:46 +0530

    login-flow-change

--- src/pages/reset-password/reset-password.ts
+++ src/pages/reset-password/reset-password.ts
@@ -1,91 +1,97 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { UtilsProvider } from '../../providers/utils/utils';
-import { AppConfigs } from '../../providers/appConfig';
-import { LocalStorageProvider } from '../../providers/local-storage/local-storage';
-
+import { Component } from "@angular/core";
+import { NavController, NavParams } from "ionic-angular";
+import { Validators, FormBuilder, FormGroup, FormControl } from "@angular/forms";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { UtilsProvider } from "../../providers/utils/utils";
+import { AppConfigs } from "../../providers/appConfig";
+import { LocalStorageProvider } from "../../providers/local-storage/local-storage";
 
 @Component({
-  selector: 'page-reset-password',
-  templateUrl: 'reset-password.html',
+  selector: "page-reset-password",
+  templateUrl: "reset-password.html",
 })
 export class ResetPasswordPage {
   private resetPasswordForm: FormGroup;
   counter = 0;
-  payload = {}
+  payload = {};
   password;
   oldPassword;
   confirmPassword;
   userDetails;
   partnerDetails;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    private formBuilder: FormBuilder, private http: HttpClient, private utils: UtilsProvider,
-    private localStorage: LocalStorageProvider) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private utils: UtilsProvider,
+    private localStorage: LocalStorageProvider
+  ) {
     this.resetPasswordForm = this.formBuilder.group({
-      oldPassword: new FormControl('', [Validators.required, Validators.maxLength(35)]),
-      password: new FormControl('', [Validators.required, Validators.maxLength(35)]),
-      confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(35)]),
+      oldPassword: new FormControl("", [Validators.required, Validators.maxLength(35)]),
+      password: new FormControl("", [Validators.required, Validators.maxLength(35)]),
+      confirmPassword: new FormControl("", [Validators.required, Validators.maxLength(35)]),
     });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ForgotPasswordPage');
-    this.localStorage.getLocalStorage('partnerLoginDetails').then(data => {
-      this.partnerDetails = data;
-    }).catch(error => {
-
-    })
+    console.log("ionViewDidLoad ForgotPasswordPage");
+    this.localStorage
+      .getLocalStorage("partnerLoginDetails")
+      .then((data) => {
+        this.partnerDetails = data;
+      })
+      .catch((error) => {});
   }
 
   encryptParams(stringToEncrypt) {
     if (this.counter === 0) {
-      this.utils.startLoader()
+      this.utils.startLoader();
     }
     const params = new HttpParams({
       fromObject: {
-        'values': stringToEncrypt ? stringToEncrypt : this.resetPasswordForm.value.password,
-        'key': AppConfigs.punjabApiKey
-      }
+        values: stringToEncrypt ? stringToEncrypt : this.resetPasswordForm.value.password,
+        key: AppConfigs.punjabApiKey,
+      },
     });
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      })
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     };
 
-    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.encryptedMethod, params, httpOptions).subscribe(success => {
-    }, error => {
-      let encryptedString = (error.error.text.split('<string xmlns="http://tempuri.org/">'))[1].split('</string>')[0];
-      if (this.counter === 0) {
-        this.password = encryptedString
-        this.counter++;
-        this.encryptParams(this.resetPasswordForm.value.confirmPassword)
-      } else if (this.counter === 1) {
-        this.confirmPassword = encryptedString
-        this.counter++;
-        this.encryptParams(this.resetPasswordForm.value.oldPassword)
-      } else if (this.counter === 2) {
-        this.oldPassword = encryptedString
-        this.counter++;
-        this.encryptParams(this.partnerDetails.facultyCode)
-      } else {
-        this.payload = {
-          password: this.password,
-          confirmPassword: this.confirmPassword,
-          oldPassword: this.oldPassword,
-          key: AppConfigs.punjabApiKey,
-          facultyCode: encryptedString
-        };
-        this.counter = 0;
-        console.log(JSON.stringify(this.payload))
-        this.resetPassword();
+    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.encryptedMethod, params, httpOptions).subscribe(
+      (success) => {},
+      (error) => {
+        let encryptedString = error.error.text.split('<string xmlns="http://tempuri.org/">')[1].split("</string>")[0];
+        if (this.counter === 0) {
+          this.password = encryptedString;
+          this.counter++;
+          this.encryptParams(this.resetPasswordForm.value.confirmPassword);
+        } else if (this.counter === 1) {
+          this.confirmPassword = encryptedString;
+          this.counter++;
+          this.encryptParams(this.resetPasswordForm.value.oldPassword);
+        } else if (this.counter === 2) {
+          this.oldPassword = encryptedString;
+          this.counter++;
+          this.encryptParams(this.partnerDetails.facultyCode);
+        } else {
+          this.payload = {
+            password: this.password,
+            confirmPassword: this.confirmPassword,
+            oldPassword: this.oldPassword,
+            key: AppConfigs.punjabApiKey,
+            facultyCode: encryptedString,
+          };
+          this.counter = 0;
+          console.log(JSON.stringify(this.payload));
+          this.resetPassword();
+        }
       }
-    })
-
+    );
   }
 
   resetPassword() {
@@ -94,45 +100,67 @@ export class ResetPasswordPage {
         password: this.password,
         oldPassword: this.oldPassword,
         confirmPassword: this.confirmPassword,
-        facultyCode: this.payload['facultyCode'],
-        key: AppConfigs.punjabApiKey
-      }
+        facultyCode: this.payload["facultyCode"],
+        key: AppConfigs.punjabApiKey,
+      },
     });
 
-
-    console.log(JSON.stringify(params))
+    console.log(JSON.stringify(params));
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      })
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     };
-    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.resetPassword, params, httpOptions).subscribe(success => {
-    }, error => {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(error.error.text, "text/xml");
-      this.utils.stopLoader();
-      try {
-        this.userDetails = JSON.parse(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue)[0];
-      } catch (e) {
-        // console.log(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue)
-        // JSON.parse(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue, (key, value) =>
-        //  {console.log(key);
-        //  console.log(value)
-        //  }
-        // );
-        // this.utils.stopLoader();
-        console.log(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue);
-        if (xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue.indexOf('Password & confirm password is not same !!!') >= 0) {
-          this.utils.openToast("Password & confirm password is not same !!!");
-
-        } else if (xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue.indexOf('Your password has chanced !!!') >= 0) {
-          this.utils.openToast("Your password has been changed");
-          this.navCtrl.pop();
-        } else {
-          this.utils.openToast("Invalid password.");
+    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.resetPassword, params, httpOptions).subscribe(
+      (success) => {},
+      (error) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(error.error.text, "text/xml");
+        this.utils.stopLoader();
+        try {
+          this.userDetails = JSON.parse(xmlDoc.getElementsByTagName("string")[0].childNodes[0].nodeValue)[0];
+        } catch (e) {
+          // console.log(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue)
+          // JSON.parse(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue, (key, value) =>
+          //  {console.log(key);
+          //  console.log(value)
+          //  }
+          // );
+          // this.utils.stopLoader();
+          console.log(xmlDoc.getElementsByTagName("string")[0].childNodes[0].nodeValue);
+          if (
+            xmlDoc
+              .getElementsByTagName("string")[0]
+              .childNodes[0].nodeValue.indexOf("Password & confirm password is not same !!!") >= 0
+          ) {
+            this.utils.openToast("Password & confirm password is not same !!!");
+          } else if (
+            xmlDoc.getElementsByTagName("string")[0].childNodes[0].nodeValue.indexOf("Your password has chanced !!!") >=
+            0
+          ) {
+            this.utils.openToast("Your password has been changed");
+            this.navCtrl.pop();
+          } else {
+            this.utils.openToast("Invalid password.");
+          }
         }
       }
-    })
+    );
   }
 
+  resetPasswordNew() {
+    let payload = this.resetPasswordForm.value;
+    payload.facultyCode = this.partnerDetails.facultyCode;
+    let url = AppConfigs.user_management_base_url + "v1" + AppConfigs.punjab.resetPasswordNew;
+
+    this.http.post(url, payload).subscribe(
+      (res) => {
+        this.utils.openToast(res["message"], "close");
+        this.navCtrl.pop();
+      },
+      (err) => {
+        this.utils.openToast("Please try again !!");
+      }
+    );
+  }
 }
--- src/pages/user-login/user-login.ts
+++ src/pages/user-login/user-login.ts
@@ -1,63 +1,73 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, App } from 'ionic-angular';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { AppConfigs } from '../../providers/appConfig';
-import { Http, URLSearchParams } from '@angular/http';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { CurrentUserProvider } from '../../providers/current-user/current-user';
-import { HomePage } from '../home/home';
-import { UtilsProvider } from '../../providers/utils/utils';
-import { ForgotPasswordPage } from '../forgot-password/forgot-password';
-import { LocalStorageProvider } from '../../providers/local-storage/local-storage';
-import { HTTP } from '@ionic-native/http';
-import { FcmProvider } from '../../providers/fcm/fcm';
-import { AuthProvider } from '../../providers/auth/auth';
+import { Component } from "@angular/core";
+import { NavController, NavParams, App } from "ionic-angular";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { AppConfigs } from "../../providers/appConfig";
+import { Http, URLSearchParams } from "@angular/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { CurrentUserProvider } from "../../providers/current-user/current-user";
+import { HomePage } from "../home/home";
+import { UtilsProvider } from "../../providers/utils/utils";
+import { ForgotPasswordPage } from "../forgot-password/forgot-password";
+import { LocalStorageProvider } from "../../providers/local-storage/local-storage";
+import { HTTP } from "@ionic-native/http";
+import { FcmProvider } from "../../providers/fcm/fcm";
+import { AuthProvider } from "../../providers/auth/auth";
 import * as jwt_decode from "jwt-decode";
+import { switchMap, catchError, flatMap, map, mergeMap } from "rxjs/operators";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'page-user-login',
-  templateUrl: 'user-login.html',
+  selector: "page-user-login",
+  templateUrl: "user-login.html",
 })
 export class UserLoginPage {
-
   private signIn: FormGroup;
   auth_url;
-  counter = 0;;
-  paylod = {}
+  counter = 0;
+  paylod = {};
   password;
   staffID;
   adminToken;
   userDetails;
   isNewUser: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private utils: UtilsProvider, private auth: AuthProvider,
-    private currentUser: CurrentUserProvider, private app: App, private navCtrl: NavController, private ionicHttp: HTTP,
-    private localStorage: LocalStorageProvider, private fcm: FcmProvider) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private utils: UtilsProvider,
+    private auth: AuthProvider,
+    private currentUser: CurrentUserProvider,
+    private app: App,
+    private navCtrl: NavController,
+    private ionicHttp: HTTP,
+    private localStorage: LocalStorageProvider,
+    private fcm: FcmProvider
+  ) {
     this.signIn = this.formBuilder.group({
-      staffID: ['', Validators.required],
-      password: ['', Validators.required],
+      staffID: ["", Validators.required],
+      password: ["", Validators.required],
     });
   }
 
   checkForKeycloakUser(username, password) {
     const params = new HttpParams({
       fromObject: {
-        grant_type: 'password',
+        grant_type: "password",
         username: username,
         password: AppConfigs.adminCredentials.userPassword,
         client_id: AppConfigs.clientId,
-        scope: "offline_access"
-      }
+        scope: "offline_access",
+      },
     });
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      })
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     };
     const url = AppConfigs.app_url + "/auth/realms/sunbird/protocol/openid-connect/token";
-    this.http.post(url, params, httpOptions)
-      .subscribe((data: any) => {
+    this.http.post(url, params, httpOptions).subscribe(
+      (data: any) => {
         if (this.isNewUser) {
           this.addUserToOrganization(data);
         } else {
@@ -74,9 +84,11 @@ export class UserLoginPage {
         //   let nav = this.app.getActiveNav();
         //   nav.setRoot(HomePage);
         // })
-      }, error => {
+      },
+      (error) => {
         this.adminLogin();
-      });
+      }
+    );
   }
 
   addUserToOrganization(userDetails) {
@@ -84,192 +96,258 @@ export class UserLoginPage {
     const url = AppConfigs.app_url + AppConfigs.keyCloak.addUser;
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         "X-authenticated-user-token": this.adminToken,
-        "Authorization": AppConfigs.adminCredentials.auterizationToken
-      })
+        Authorization: AppConfigs.adminCredentials.auterizationToken,
+      }),
     };
     const userId = userDetails.access_token ? jwt_decode(userDetails.access_token).sub : "";
     const params = {
-      "params": {},
-      "request": {
-        "userId": userId,
-        "organisationId": AppConfigs.keyCloak.punjabOrgId,
-        "roles": AppConfigs.keyCloak.roles
-      }
-    }
-    this.http.post(url, params, httpOptions)
-      .subscribe((data: any) => {
+      params: {},
+      request: {
+        userId: userId,
+        organisationId: AppConfigs.keyCloak.punjabOrgId,
+        roles: AppConfigs.keyCloak.roles,
+      },
+    };
+    this.http.post(url, params, httpOptions).subscribe(
+      (data: any) => {
         this.utils.stopLoader();
         this.auth.checkForCurrentUserLocalData(userDetails);
         this.isNewUser = false;
-      }, error => {
+      },
+      (error) => {
         this.utils.stopLoader();
         this.utils.openToast("Unable to login. Please try again");
         this.isNewUser = true;
-      });
+      }
+    );
   }
 
   adminLogin() {
     const params = new HttpParams({
       fromObject: {
-        grant_type: 'password',
+        grant_type: "password",
         username: AppConfigs.adminCredentials.username,
         password: AppConfigs.adminCredentials.password,
-        client_id: AppConfigs.clientId
-      }
+        client_id: AppConfigs.clientId,
+      },
     });
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      })
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     };
     const url = AppConfigs.app_url + "/auth/realms/sunbird/protocol/openid-connect/token";
-    this.http.post(url, params, httpOptions)
-      .subscribe((data: any) => {
+    this.http.post(url, params, httpOptions).subscribe(
+      (data: any) => {
         this.adminToken = data.access_token;
         this.createUser();
-      }, error => {
-        console.log(JSON.stringify(error))
+      },
+      (error) => {
+        console.log(JSON.stringify(error));
         this.utils.stopLoader();
         this.utils.openToast("Please try again.");
-      });
+      }
+    );
   }
 
   createUser() {
     const body = {
-      "params": {},
-      "request": {
-        "channel": AppConfigs.adminCredentials.adminChanel,
-        "firstName": this.userDetails.staffName,
-        "lastName": "",
-        "email": this.signIn.value.staffID + AppConfigs.adminCredentials.userEmailExtension,
-        "emailVerified": true,
-        "userName": this.signIn.value.staffID,
-        "password": AppConfigs.adminCredentials.userPassword
-      }
-
-    }
+      params: {},
+      request: {
+        channel: AppConfigs.adminCredentials.adminChanel,
+        firstName: this.userDetails.staffName,
+        lastName: "",
+        email: this.signIn.value.staffID + AppConfigs.adminCredentials.userEmailExtension,
+        emailVerified: true,
+        userName: this.signIn.value.staffID,
+        password: AppConfigs.adminCredentials.userPassword,
+      },
+    };
 
     const url = AppConfigs.app_url + AppConfigs.punjab.createUser;
 
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         "x-authenticated-user-token": this.adminToken,
-        "Authorization": AppConfigs.adminCredentials.auterizationToken
-      })
+        Authorization: AppConfigs.adminCredentials.auterizationToken,
+      }),
     };
 
-    this.http.post(url, body, httpOptions)
-      .subscribe((data: any) => {
+    this.http.post(url, body, httpOptions).subscribe(
+      (data: any) => {
         this.isNewUser = true;
-        this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password)
-      }, error => {
+        this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password);
+      },
+      (error) => {
         this.utils.stopLoader();
         this.utils.openToast("Invalid UserID & Password !!!");
-      });
-
+      }
+    );
   }
 
   logForm() {
     const params = new HttpParams({
       fromObject: {
-        staffID: this.paylod['staffID'],
-        password: this.paylod['password'],
-        key: AppConfigs.punjabApiKey
-      }
+        staffID: this.paylod["staffID"],
+        password: this.paylod["password"],
+        key: AppConfigs.punjabApiKey,
+      },
     });
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-      })
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
     };
-    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.login, params, httpOptions).subscribe(success => {
-    }, error => {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(error.error.text, "text/xml");
-      try {
-        this.userDetails = JSON.parse(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue)[0];
-        this.localStorage.setLocalStorage("partnerLoginDetails", this.userDetails);
-        this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password)
-      } catch (e) {
-        this.utils.stopLoader();
-        this.utils.openToast("Invalid UserID & Password !!!");
+    this.http.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.login, params, httpOptions).subscribe(
+      (success) => {},
+      (error) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(error.error.text, "text/xml");
+        try {
+          this.userDetails = JSON.parse(xmlDoc.getElementsByTagName("string")[0].childNodes[0].nodeValue)[0];
+          this.localStorage.setLocalStorage("partnerLoginDetails", this.userDetails);
+          this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password);
+        } catch (e) {
+          this.utils.stopLoader();
+          this.utils.openToast("Invalid UserID & Password !!!");
+        }
       }
-    })
+    );
   }
 
   login() {
     const payload = {
-      staffID: this.paylod['staffID'],
-      password: this.paylod['password'],
-      key: AppConfigs.punjabApiKey
+      staffID: this.paylod["staffID"],
+      password: this.paylod["password"],
+      key: AppConfigs.punjabApiKey,
     };
     const obj = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    }
-    this.ionicHttp.setDataSerializer('urlencoded');
-    this.ionicHttp.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.login, payload, obj).then(success => {
-      const parser = new DOMParser();
-      const xmlDoc = parser.parseFromString(success.data, "text/xml");
-      try {
-        this.userDetails = JSON.parse(xmlDoc.getElementsByTagName('string')[0].childNodes[0].nodeValue)[0];
-        this.localStorage.setLocalStorage("partnerLoginDetails", this.userDetails);
-        this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password)
-      } catch (e) {
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    this.ionicHttp.setDataSerializer("urlencoded");
+    this.ionicHttp
+      .post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.login, payload, obj)
+      .then((success) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(success.data, "text/xml");
+        try {
+          this.userDetails = JSON.parse(xmlDoc.getElementsByTagName("string")[0].childNodes[0].nodeValue)[0];
+          this.localStorage.setLocalStorage("partnerLoginDetails", this.userDetails);
+          this.checkForKeycloakUser(this.signIn.value.staffID, this.signIn.value.password);
+        } catch (e) {
+          this.utils.stopLoader();
+          this.utils.openToast("Invalid UserID & Password !!!");
+        }
+      })
+      .catch((error) => {
+        this.signIn.reset();
         this.utils.stopLoader();
-        this.utils.openToast("Invalid UserID & Password !!!");
-      }
-    }).catch(error => {
-      this.signIn.reset();
-      this.utils.stopLoader();
-      this.paylod = {};
-      console.log(JSON.stringify(error));
-      this.staffID = ""
-      this.utils.openToast(error.error);
-    })
+        this.paylod = {};
+        console.log(JSON.stringify(error));
+        this.staffID = "";
+        this.utils.openToast(error.error);
+      });
   }
 
   goToForgotPassword() {
-    this.navCtrl.push(ForgotPasswordPage)
+    this.navCtrl.push(ForgotPasswordPage);
   }
 
   encryptParams(stringToEncrypt) {
     if (this.counter === 0) {
-      this.utils.startLoader()
+      this.utils.startLoader();
     }
     const payload = {
-      'values': stringToEncrypt ? stringToEncrypt : this.signIn.value.staffID,
-      'key': AppConfigs.punjabApiKey
+      values: stringToEncrypt ? stringToEncrypt : this.signIn.value.staffID,
+      key: AppConfigs.punjabApiKey,
     };
 
-    this.ionicHttp.setDataSerializer('urlencoded');
+    this.ionicHttp.setDataSerializer("urlencoded");
 
-    this.ionicHttp.post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.encryptedMethod, payload, {}).then(success => {
-      let encryptedString = (success.data.split('<string xmlns="http://tempuri.org/">'))[1].split('</string>')[0];
-      if (this.counter === 0) {
-        this.staffID = encryptedString
-        this.counter++;
-        this.encryptParams(this.signIn.value.password)
-      } else {
-        this.paylod = {
-          staffID: this.staffID,
-          password: encryptedString,
-          key: AppConfigs.punjabApiKey
-        };
-        console.log(JSON.stringify(this.paylod))
-        this.counter = 0;
-        // this.isNewUser = true;
-        this.login();
-      }
-    }).catch(error => {
-      this.utils.stopLoader();
-      let errorMessage = ((error.status === 500) || (error.status === 400)) ? error.error : "Unable to fetch user details. Please contact the support team for assistance.";
-      this.utils.openToast(errorMessage);
-    })
-
+    this.ionicHttp
+      .post(AppConfigs.punjabBaseUrl + AppConfigs.punjab.encryptedMethod, payload, {})
+      .then((success) => {
+        let encryptedString = success.data.split('<string xmlns="http://tempuri.org/">')[1].split("</string>")[0];
+        if (this.counter === 0) {
+          this.staffID = encryptedString;
+          this.counter++;
+          this.encryptParams(this.signIn.value.password);
+        } else {
+          this.paylod = {
+            staffID: this.staffID,
+            password: encryptedString,
+            key: AppConfigs.punjabApiKey,
+          };
+          console.log(JSON.stringify(this.paylod));
+          this.counter = 0;
+          // this.isNewUser = true;
+          this.login();
+        }
+      })
+      .catch((error) => {
+        this.utils.stopLoader();
+        let errorMessage =
+          error.status === 500 || error.status === 400
+            ? error.error
+            : "Unable to fetch user details. Please contact the support team for assistance.";
+        this.utils.openToast(errorMessage);
+      });
   }
 
+  // only using this to login now .
+  loginNew() {
+    this.utils.startLoader();
+
+    const params = new HttpParams({
+      fromObject: {
+        grant_type: "password",
+        username: AppConfigs.adminCredentials.username,
+        password: AppConfigs.adminCredentials.password,
+        client_id: AppConfigs.clientId,
+      },
+    });
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
+    };
+    let url = AppConfigs.app_url + "/auth/realms/sunbird/protocol/openid-connect/token";
+    this.http
+      .post(url, params, httpOptions)
+      .pipe(
+        mergeMap((data: any) => {
+          this.adminToken = data["access_token"];
+          const httpOptions = {
+            headers: new HttpHeaders({
+              "Content-Type": "application/json",
+              "x-authenticated-user-token": this.adminToken,
+              Authorization: AppConfigs.adminCredentials.auterizationToken,
+            }),
+          };
+          let payload = this.signIn.value;
+          let url = AppConfigs.user_management_base_url + "v1" + AppConfigs.punjab.staffLogin;
+          return this.http.post(url, payload, httpOptions);
+        })
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+          this.utils.stopLoader();
+          !res["result"] ? this.utils.openToast(res["message"]) : this.storeUserData(res["result"]);
+        },
+        (err) => {
+          this.utils.stopLoader();
+        }
+      );
+  }
+
+  storeUserData(data) {
+    this.localStorage.setLocalStorage("partnerLoginDetails", data);
+    this.auth.checkForCurrentUserLocalData(data.tokenDetails);
+  }
 }
