ee02f996de7757992ef883eae87dc6b75b06c58b
rejneesh1 <pillairejneesh@gmail.com>
2020-08-03 09:14:08 +0530

    STORY#619

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,14 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CamelcasePipe } from './camelcase/camelcase';
-import { GetLabelsPipe } from './get-labels/get-labels';
-import { SearchPipe } from './search/search';
+import { NgModule } from "@angular/core";
+import { CamelcasePipe } from "./camelcase/camelcase";
+import { GetLabelsPipe } from "./get-labels/get-labels";
+import { SearchPipe } from "./search/search";
+import { criteriaFilterPipe } from "../pages/manual-rating/manual-rating";
 @NgModule({
-	declarations: [CamelcasePipe,
-    GetLabelsPipe,
-    SearchPipe],
-	imports: [],
-	exports: [CamelcasePipe,
-    GetLabelsPipe,
-    SearchPipe]
+  declarations: [CamelcasePipe, GetLabelsPipe, SearchPipe, criteriaFilterPipe],
+  imports: [],
+  exports: [CamelcasePipe, GetLabelsPipe, SearchPipe, criteriaFilterPipe],
 })
 export class PipesModule {}
